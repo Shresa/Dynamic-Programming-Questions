b27e23adf7f99ad9b086541af9c6f8f5807ed423
Shreshtha Sadhu <84222590+Shresa@users.noreply.github.com>
2022-10-28 11:45:29 +0530

    Update lcs.cpp
    Code uploaded

--- lcs.cpp
+++ lcs.cpp
@@ -1,1 +1,27 @@
-
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+  
+int lcs( char *X, char *Y, int m, int n ) 
+{ 
+    if (m == 0 || n == 0) 
+        return 0; 
+    if (X[m-1] == Y[n-1]) 
+        return 1 + lcs(X, Y, m-1, n-1); 
+    else
+        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n)); 
+} 
+  
+  
+ 
+int main() 
+{ 
+    string str1,str2;
+    cin>>str1>>str2;
+    int m = str1.size(); 
+    int n = str2.size(); 
+      
+    cout<<"Length of LCS is "<< lcs( X, Y, m, n ) ; 
+      
+    return 0; 
+}
